55e62329e0f8233718a1b9da59b74cef7d703591
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-04 21:21:50 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -6,17 +7,35 @@ class BasePage:
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
+    @allure.step("Кликнуть на элемент {locator}")
     def click(self, locator):
         self.wait.until(EC.element_to_be_clickable(locator)).click()
 
+    @allure.step("Ввести '{text}' в поле {locator}")
     def send_keys(self, locator, text):
         element = self.wait.until(EC.visibility_of_element_located(locator))
         element.clear()
         element.send_keys(text)
 
+    @allure.step("Получить текст элемента {locator}")
     def get_text(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator)).text
 
+    @allure.step("Скролл до элемента {locator}")
     def scroll_to_element(self, locator):
         element = self.driver.find_element(*locator)
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
+
+    @allure.step("Получить текущий URL")
+    def get_current_url(self):
+        return self.driver.current_url
+
+    @allure.step("Переключиться на новое окно")
+    def switch_to_new_window(self):
+        self.wait.until(lambda d: len(d.window_handles) > 1)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+
+    @allure.step("Дождаться появления нового окна и вернуть его URL")
+    def get_new_window_url(self):
+        self.switch_to_new_window()
+        return self.driver.current_url
