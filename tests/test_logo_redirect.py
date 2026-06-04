6816a4f22fc73f86d4151d4517bd247a0cedb180
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-04 21:06:43 +0300

    Update test_logo_redirect.py

--- tests/test_logo_redirect.py
+++ tests/test_logo_redirect.py
@@ -1,23 +1,20 @@
 import allure
-from selenium.webdriver.support import expected_conditions as EC
 from pages.main_page import MainPage
+from data.urls import BASE_URL, ORDER_PAGE_URL, DZEN_URL_PATTERN
 
 @allure.feature("Логотипы")
 class TestLogoRedirect:
     @allure.title("Клик по логотипу 'Самокат' возвращает на главную страницу")
     def test_scooter_logo_redirects_to_main(self, driver):
-        driver.get("https://qa-scooter.praktikum-services.ru/order")
         main_page = MainPage(driver)
+        driver.get(ORDER_PAGE_URL)           # переход на страницу заказа
         main_page.click_scooter_logo()
-        assert driver.current_url == "https://qa-scooter.praktikum-services.ru/"
+        assert main_page.get_current_url() == BASE_URL
 
     @allure.title("Клик по логотипу 'Яндекс' открывает Дзен в новой вкладке")
     def test_yandex_logo_redirects_to_dzen(self, driver):
-        driver.get("https://qa-scooter.praktikum-services.ru/")
         main_page = MainPage(driver)
+        main_page.open()
         main_page.click_yandex_logo()
-        # переключаемся на новую вкладку
-        driver.switch_to.window(driver.window_handles[1])
-        # ждём редирект на dzen.ru
-        driver.get("https://dzen.ru/")  # или проверить, что URL содержит dzen.ru
-        assert "dzen.ru" in driver.current_url
+        new_window_url = main_page.get_new_window_url()
+        assert DZEN_URL_PATTERN in new_window_url
