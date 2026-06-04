e7900ce1bbe56b72cea4f4f1dec931cbb5f6d1c6
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-04 21:07:37 +0300

    Update test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -2,50 +2,52 @@ import allure
 import pytest
 from pages.main_page import MainPage
 from pages.order_page import OrderPage
+from data.urls import BASE_URL
+
+# Данные для заказа (два набора)
+ORDER_DATA_SET_1 = {
+    "name": "Николай",
+    "surname": "Кузнецов",
+    "address": "ул. Пушкина, 10",
+    "metro": "Сокольники",
+    "phone": "+79998887766",
+    "date": "10.06.2025",
+    "rental_period": "сутки",
+    "color": "black",
+    "comment": "Позвонить за час"
+}
+
+ORDER_DATA_SET_2 = {
+    "name": "Екатерина",
+    "surname": "Иванова",
+    "address": "пр. Мира, 5",
+    "metro": "Комсомольская",
+    "phone": "+79161234567",
+    "date": "15.06.2025",
+    "rental_period": "двое суток",
+    "color": "grey",
+    "comment": "Домофон 123"
+}
 
 @allure.feature("Заказ самоката")
 class TestOrder:
-    @pytest.mark.parametrize("order_data, button_position", [
-        # первый набор данных, кнопка вверху
-        (
-            {
-                "name": "Николай",
-                "surname": "Кузнецов",
-                "address": "ул. Пушкина, 10",
-                "metro": "Сокольники",
-                "phone": "+79998887766",
-                "date": "10.06.2025",
-                "rental_period": "сутки",
-                "color": "black",
-                "comment": "Позвонить за час"
-            },
-            "top"
-        ),
-        # второй набор данных, кнопка внизу
-        (
-            {
-                "name": "Екатерина",
-                "surname": "Иванова",
-                "address": "пр. Мира, 5",
-                "metro": "Комсомольская",
-                "phone": "+79161234567",
-                "date": "15.06.2025",
-                "rental_period": "двое суток",
-                "color": "grey",
-                "comment": "Домофон 123"
-            },
-            "bottom"
-        )
-    ])
-    @allure.title("Позитивный сценарий заказа с кнопкой '{button_position}'")
-    def test_successful_order(self, driver, order_data, button_position):
-        driver.get("https://qa-scooter.praktikum-services.ru/")
+    @allure.title("Позитивный сценарий заказа через верхнюю кнопку")
+    @pytest.mark.parametrize("order_data", [ORDER_DATA_SET_1, ORDER_DATA_SET_2])
+    def test_successful_order_top_button(self, driver, order_data):
         main_page = MainPage(driver)
-        if button_position == "top":
-            main_page.click_order_button_top()
-        else:
-            main_page.click_order_button_bottom()
+        main_page.open()
+        main_page.click_order_button_top()
+        self._complete_order(driver, order_data)
 
+    @allure.title("Позитивный сценарий заказа через нижнюю кнопку")
+    @pytest.mark.parametrize("order_data", [ORDER_DATA_SET_1, ORDER_DATA_SET_2])
+    def test_successful_order_bottom_button(self, driver, order_data):
+        main_page = MainPage(driver)
+        main_page.open()
+        main_page.click_order_button_bottom()
+        self._complete_order(driver, order_data)
+
+    def _complete_order(self, driver, order_data):
         order_page = OrderPage(driver)
         order_page.fill_first_form(
             order_data["name"], order_data["surname"], order_data["address"],
