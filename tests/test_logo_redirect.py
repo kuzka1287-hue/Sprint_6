3fa5958fd3727820edd49033bf5f55420d288f8f
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-06 10:14:38 +0300

    Update test_logo_redirect.py

--- tests/test_logo_redirect.py
+++ tests/test_logo_redirect.py
@@ -7,15 +7,14 @@ class TestLogoRedirect:
     @allure.title("Клик по логотипу 'Самокат' возвращает на главную страницу")
     def test_scooter_logo_redirects_to_main(self, driver):
         main_page = MainPage(driver)
-        driver.get(ORDER_PAGE_URL)           # переход на страницу заказа
+        main_page.open(ORDER_PAGE_URL)        # используем метод open из BasePage
         main_page.click_scooter_logo()
         assert main_page.get_current_url() == BASE_URL
 
     @allure.title("Клик по логотипу 'Яндекс' открывает Дзен в новой вкладке")
     def test_yandex_logo_redirects_to_dzen(self, driver):
         main_page = MainPage(driver)
-        main_page.open()
+        main_page.open_main()
         main_page.click_yandex_logo()
         new_window_url = main_page.get_new_window_url()
         assert DZEN_URL_PATTERN in new_window_url
- 
