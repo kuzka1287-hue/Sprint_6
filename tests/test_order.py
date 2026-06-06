88236c989befc4bcb91a4b72f9b48b777f4f884f
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-06 10:15:32 +0300

    Update test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -2,7 +2,6 @@ import allure
 import pytest
 from pages.main_page import MainPage
 from pages.order_page import OrderPage
-from data.urls import BASE_URL
 
 # Данные для заказа (два набора)
 ORDER_DATA_SET_1 = {
@@ -35,19 +34,8 @@ class TestOrder:
     @pytest.mark.parametrize("order_data", [ORDER_DATA_SET_1, ORDER_DATA_SET_2])
     def test_successful_order_top_button(self, driver, order_data):
         main_page = MainPage(driver)
-        main_page.open()
+        main_page.open_main()
         main_page.click_order_button_top()
-        self._complete_order(driver, order_data)
-
-    @allure.title("Позитивный сценарий заказа через нижнюю кнопку")
-    @pytest.mark.parametrize("order_data", [ORDER_DATA_SET_1, ORDER_DATA_SET_2])
-    def test_successful_order_bottom_button(self, driver, order_data):
-        main_page = MainPage(driver)
-        main_page.open()
-        main_page.click_order_button_bottom()
-        self._complete_order(driver, order_data)
-
-    def _complete_order(self, driver, order_data):
         order_page = OrderPage(driver)
         order_page.fill_first_form(
             order_data["name"], order_data["surname"], order_data["address"],
@@ -58,5 +46,24 @@ class TestOrder:
             order_data["color"], order_data["comment"]
         )
         order_page.confirm_order()
-        success_msg = order_page.get_success_message() 
+        success_msg = order_page.get_success_message()
+        assert "Заказ оформлен" in success_msg
+
+    @allure.title("Позитивный сценарий заказа через нижнюю кнопку")
+    @pytest.mark.parametrize("order_data", [ORDER_DATA_SET_1, ORDER_DATA_SET_2])
+    def test_successful_order_bottom_button(self, driver, order_data):
+        main_page = MainPage(driver)
+        main_page.open_main()
+        main_page.click_order_button_bottom()
+        order_page = OrderPage(driver)
+        order_page.fill_first_form(
+            order_data["name"], order_data["surname"], order_data["address"],
+            order_data["metro"], order_data["phone"]
+        )
+        order_page.fill_second_form(
+            order_data["date"], order_data["rental_period"],
+            order_data["color"], order_data["comment"]
+        )
+        order_page.confirm_order()
+        success_msg = order_page.get_success_message()
         assert "Заказ оформлен" in success_msg
